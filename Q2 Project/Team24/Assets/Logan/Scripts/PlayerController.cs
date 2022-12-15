1522009ad1777af86914d583b4946de6f22f615f
MUKILTEO\1622506 <averagememe2022@gmail.com>
2022-12-15 12:45:50 -0800

    Dialogue and Character Controller Complete

--- Q2 Project/Team24/Assets/Logan/Scripts/PlayerController.cs
+++ Q2 Project/Team24/Assets/Logan/Scripts/PlayerController.cs
@@ -5,21 +5,57 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2D;
+
+    private float moveSpeed;
+    private float jumpForce;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+
+        moveSpeed = 3f;
+        jumpForce = 60f;
+        isJumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate()
     {
-        
+        if (moveHorizontal > 0.1f || moveHorizontal < -0.1f)
+        {
+            rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+
+        if (!isJumping && moveVertical > 0.1f)
+        {
+            rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = true;
+        }
     }
 }
