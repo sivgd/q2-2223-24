cd1db078163b201e73f2f34dfc05731d86b7371e
MUKILTEO\1622506 <averagememe2022@gmail.com>
2023-01-03 12:12:50 -0800

    Lesbiabs can now talk

---
+++ Q2 Project/Team24/Assets/Logan/Scripts/Dialogue/LesbiabsDialog.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class LesbiabsDialog: MonoBehaviour
+{
+    public GameObject dialogBox;
+    public TMP_Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if (dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+
+}
