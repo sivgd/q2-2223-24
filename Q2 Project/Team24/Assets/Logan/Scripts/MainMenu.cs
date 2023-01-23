ab301c1abf3c1e226fe803d1f30c17014edec902
MUKILTEO\1622506 <averagememe2022@gmail.com>
2023-01-23 13:16:52 -0800

    made an entire AAA game by myself :)

--- Q2 Project/Team24/Assets/Logan/Scripts/MainMenu.cs
+++ Q2 Project/Team24/Assets/Logan/Scripts/MainMenu.cs
@@ -20,6 +20,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
+    public void Programmers()
+    {
+        SceneManager.LoadScene("CreditsProgrammers");
+    }
+
     public void QuitGame()
     {
         Application.Quit();
